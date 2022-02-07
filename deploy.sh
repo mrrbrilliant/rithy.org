4013557513992f12d597772c9b1c3c692fc0ed89
mrrbrilliant <mrrbrilliantwork@gmail.com>
2022-02-07 14:54:42 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 git submodule update --init --recursive
-hugo
-sudo rm -rf /var/www/rithy.org
-sudo cp -r public /var/www/rithy.org
-cd /var/www
+hugo --minify
+
+if [[ $1 == "dev" ]]; then
+	sudo rm -rf /var/www/rithy.org
+	sudo cp -r public /var/www/rithy.org
+	cd /var/www
+else
+	sudo rm -rf /kmp/www/rithythul/rithy.org
+	sudo cp -r public /kmp/www/rithythul
+	cd /kmp/www/rithythul
+fi
+
 sudo chmod 755 rithy.org
 sudo find rithy.org -type f -exec chmod 644 {} \;
 sudo find rithy.org -type d -exec chmod 755 {} \;
